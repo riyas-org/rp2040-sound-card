4942bfda290ab351fa67886846a888926ce4fdb9
riyas-org <risu.vk@gmail.com>
2026-06-23 15:33:44 +0200

    all works, cat,ft8,audio rx

--- lb7ug_ft8/lb7ug_ft8_v6/hal/si5351.h
+++
@@ -1,9 +0,0 @@
-// hal/si5351.h
-#pragma once
-#include <stdint.h>
-#include <stdbool.h>
-
-void si5351_init(void);
-void si5351_enable(uint8_t clk, bool on);
-void si5351_set_freq(uint8_t clk, uint64_t freq_hz, uint8_t drive_ma);
-extern int32_t si5351_cal_ppb;   // calibration in ppb (optional)
